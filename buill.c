008d58b71fe2f3de2483b20fa7f7f0cfda648388
iluvkim <kimbvundura@gmail.com>
2023-05-26 07:16:29 -0700

    new

--- buill.c
+++ buill.c
@@ -2,43 +2,67 @@
 
 void my_exit(const char *status)
 {
-	int exit_status = atoi(status);
+char *endptr;
+int exit_status = strtol(status, &endptr, 10);
 
-	if (exit_status != 0 || strcmp(status, "0") == 0)
-	{
-		exit(exit_status);
-	} else
-	{
-		fprintf(stderr, "Invalid exit status: %s\n", status);
-		exit(1);
-	}
+if (*endptr != '\0')
+{
+fprintf(stderr, "Invalid exit status: %s\n", status);
+exit(1);
+}
+exit(exit_status);
 }
 
 int _statuss(int argc, char *argv[])
 {
-	int num;
+int num;
 
-	if (argc > 1 && strcmp(argv[1], "exit") == 0)
-	{
-		if (argc > 2)
-		{
-			my_exit(argv[2]);
-		} else
-		{
-			fprintf(stderr, "Usage: exit status\n");
-			exit(1);
-		}
-	} else
-	{
-		printf("my shell script.\n");
+if (argc > 1 && strcmp(argv[1], "exit") == 0)
+{
+if (argc > 2)
+        {
+            my_exit(argv[2]);
+        }
+        else
+        {
+            fprintf(stderr, "Usage: exit status\n");
+            exit(1);
+        }
+    }
+    else if (argc > 1 && strcmp(argv[1], "status") == 0)
+    {
+        if (argc > 2)
+        {
+            int exit_status = atoi(argv[2]);
+            exit(exit_status);
+        }
+        else
+        {
+            fprintf(stderr, "Usage: status <exit_status>\n");
+            exit(1);
+        }
+    }
+    else if (argc > 1 && strcmp(argv[1], "fail") == 0)
+    {
+        fprintf(stderr, "Command failed.\n");
+        exit(1);
+    }
+    else
+    {
+        printf("my shell script.\n");
 
-		num = 6;
-		while (num > 0)
-		{
-			printf("long Loop: %d\n", num);
-			num--;
-		}
-	}
+        num = 6;
+        while (num > 0)
+        {
+            printf("long Loop: %d\n", num);
+            num--;
+        }
+    }
 
-	return (0);
+    return 0;
+}
+
+int mark(int argc, char *argv[])
+{
+return _statuss(argc, argv);
 }
