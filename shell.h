8c91a5be30b7093116d53abce4710edaaaa49710
iluvkim <kimbvundura@gmail.com>
2023-05-26 18:51:51 -0700

    fixup

--- shell.h
+++ shell.h
@@ -1,6 +1,3 @@
-#ifndef SHELL_H_
-#define SHELL_H_
-
 #define MAIN_H
 #define EXIT_CODE 1080
 
@@ -11,8 +8,10 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-
 #define MAX_COMMAND_LENGTH 1000
+#ifndef LISTS_H_
+#define LISTS_H_
+
 
 /**
  * struct listtoken_s - singly linked list
@@ -22,10 +21,11 @@
  */
 typedef struct listtoken_s
 {
-char *token;
-int token_length;
-struct listtoken_s *next;
+	char *token;
+	int token_length;
+	struct listtoken_s *next;
 } listtoken_t;
+
 
 /**
  * struct builtin_command - Short description
@@ -34,8 +34,8 @@ struct listtoken_s *next;
  */
 struct builtin_command
 {
-char *name;
-int (*function)(char **a);
+	char *name;
+	int (*function)(char **a);
 };
 
 size_t _tokennum(char *istr, char *delim);
@@ -47,17 +47,17 @@ int exitbuilt(char **a);
 void ffree(char **a, char *istr);
 void wfree(char **path_var, char **a);
 char *_getenv(const char *name);
-ssize_t _getLine(char **istr, size_t *nummallocalloc, int status);
+ssize_t _getLine(char **istr,size_t *nummallocalloc, int status);
 int isbuilt(char **a);
 int _handler(char **a);
 size_t listslen(const listtoken_t *head);
 void freeslist(listtoken_t **head);
+int main(__attribute((unused)) int ac,__attribute((unused)) char **arvs,__attribute((unused)) char **envp);
 size_t p_element(const listtoken_t *head);
 void inter_mode(void);
 int forks_thread(char *s, char **a);
 int print_node(void);
-void c_envar(char **dir, unsigned int envar_length,
-		const char *name, const char *value);
+void c_envar(char **dir, unsigned int envar_length, const char *name, const char *value);
 int _envset(char *dir, unsigned int envar_length, const char *name, int o);
 int _envariable(char *dir, unsigned int envar_length, unsigned int q);
 int _setenv(const char *name, const char *value, int o);
@@ -74,9 +74,7 @@ int _unsetenv(const char *name);
 int _validpath(char *filename);
 void filepath_(char **_path, char **a, char *filename, int y);
 char *_alai(char *filename);
-void my_exit(const char *status);
+void my_exit(const char* status);
 int _statuss(int argc, char *argv[]);
 
-#endif /* LISTS_H_ */
-
-#endif /* SHELL_H_ */
+#endif
